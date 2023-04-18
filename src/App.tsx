88b2a5b11c7fb816302a787f02f82e084f57d2c3
Runekid <bugdayci.onur@aol.com>
2023-04-18 13:38:13 +0200

    initial app setup

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+
+export function App() {
+  const [page, setPage] = useState<string>('home');
+
+  return (
+    <div className="App">
+      <Header setPage={setPage} />
+      <div className="container" style={{paddingTop: 36, paddingBottom: 36}}>
+        <div className="row">
+          <div>{page}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+type HeaderProps = {
+  setPage: (page: string) => void;
+}
+
+function Header({setPage}: HeaderProps) {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" onClick={() => setPage('home')}>
+          <img src="/favicon.png" style={{marginRight: 18, width: 24}} alt="itenium logo" />
+          React charts
+        </a>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" onClick={() => setPage('sample-card')}>Enquete</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
